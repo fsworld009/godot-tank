9f44bc757d6848a150a17333064b2a7814d72196
WorldFS <fsworld009@gmail.com>
2023-08-17 21:06:06 +0800

    redo stage, basic UI

--- res/game.gd
+++ res/game.gd
@@ -2,19 +2,14 @@ extends Node2D
 
 
 var enemy_list
-
-
-var spown_positions: Array[Vector2] = [
-	Vector2(96, 16)
-	Vector2(320, 16),
-	Vector2(544, 16)
-]
+var p1_live_label: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	p1_live_label = $UI/P1LiveLabel
+	print(p1_live_label)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	p1_live_label.text = str($Generators/Player1Generator.lives)
